cbf1833598c17089f92b0c1de9702d01e51022f4
Valery Zonov <valery.zonov@sibers.com>
2023-03-24 15:41:51 +0700

    quick create Map

--- app.js
+++ app.js
@@ -1,35 +1,19 @@
 'use strict';
 
-const wetherMap = new Map();
-wetherMap.set('London', '10');
-wetherMap.set('Moscow', '7');
-wetherMap
-	.set('Barnaul', '10')
-	.set('Minsk', '7');
-
-console.log(wetherMap);
-console.log(wetherMap.get('London'));
-console.log(wetherMap.get('No'));
-
-console.log(wetherMap.has('London'));
-
-wetherMap.delete('London');
-
-wetherMap.clear();
-
-
-wetherMap
-	.set(1, 5)
-	.set(true, 'Yes')
-	.set(true, 'Yes!!!')
-	.set(false, 'No')
-	.set([1, 2, 3], 'array')
-	.set({a: 1}, {b: 1});
-
-console.log(wetherMap.size);
+const wetherMap = new Map([
+	['London', 10],
+	['Moscow', 12],
+]);
 
 console.log(wetherMap);
 
-console.log(wetherMap.get([1, 2, 3]));
-console.log(wetherMap.get({a: 1}));
+const weather = {
+	london: 10,
+	moscow: 7,
+	paris: 14
+}
 
+console.log(Object.entries(weather));
+
+const weatheMap2 = new Map(Object.entries(weather));
+console.log(weatheMap2); 
