b42aace131858ebcd05b544c0ded86fa2ebfcd41
Valery Zonov <valery.zonov@sibers.com>
2023-03-27 13:22:42 +0700

    timer

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 'use strict';
 
-console.log(1);
+console.log(performance.now());
 
-const boomTimer = setTimeout((message1, message2) => {
-	console.log(message1);
-	console.log(message2);
-}, 1000, 'Bash', 'boom')
-
-clearTimeout(boomTimer);// close timer
-
-console.log(2);
+setTimeout(() => {
+	console.log(performance.now());
+}, 1000);
