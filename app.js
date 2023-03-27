6e9ba7e4272b1afe23dda20de4b5ff118ff0002c
Valery Zonov <valery.zonov@sibers.com>
2023-03-27 12:02:33 +0700

    Practice

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 'use strict';
 
-console.log(Math.round(1.4));
-console.log(Math.round(1.6));
+function random(min, max) {
+	return Math.floor(Math.random() * (max - min +1) + min);
+}
 
-console.log(Math.ceil(1.1));
-
-console.log(Math.floor(1.7));
-
-
-console.log(Math.trunc(2.5));
-console.log(Math.trunc(2.4));
-console.log(Math.trunc(2.9));
-
-console.log((1.4999999).toFixed(1));
-console.log((1.4999999).toFixed(4));
-console.log(Number((1.4999999).toFixed(4)));
+console.log(random(1, 5));
