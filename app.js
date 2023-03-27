dacc4d3d0cfc35ae8651f964ee15482de67ed8cc
Valery Zonov <valery.zonov@sibers.com>
2023-03-27 11:29:05 +0700

    WeakSet

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
 'use strict';
 
-let a = { a: 1 };
-let b = { b: 1 };
-
-const map = new WeakMap();
-map.set(a, 'test A');
-map.set(b, 'test B');
-console.log(map);
-console.log(map.get(a));
-console.log(map.has(a));
+let a = {a: 1};
+let b = {b: 2};
+const set = new WeakSet([a, b]);
 
 a = null;
+
 setTimeout(() => {
-	console.log(map);
+	console.log(set);
 }, 1000)
-
-let cache = new WeakMap();
-
-function getValue(obj) {
-	if (!cache.has(obj)) {
-		const result = 1;
-		cache.set(obj, result)
-	}
-	return cache.get(obj);
-}
-
-const res = getValue(b);
-console.log(res);
 
