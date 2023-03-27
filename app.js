d50719f576b141ef3da1523f9656872c3f04ce4e
Valery Zonov <valery.zonov@sibers.com>
2023-03-27 12:08:50 +0700

    100_500

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 'use strict';
 
-console.log(15 / 2);
-console.log(15 % 2);
-console.log(14 % 2);
+const bigNum = 350_500_012_350;
+const bigNum2 = 350500012350;
+console.log(bigNum === bigNum2);
 
+const payment = 20_10;
 
-const isEven = n => n % 2 === 0;
-console.log(isEven(2));
+console.log(Number('350_500_012_350'));
+console.log(Number('350500012350'));
