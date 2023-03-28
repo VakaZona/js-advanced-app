28bb65bb4cb916287225f80d96c9c5beac0efb21
Valery Zonov <valery.zonov@sibers.com>
2023-03-28 17:15:58 +0700

    Inheritance in ES6

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 'use strict';
 
-const Book = function (title, author) {
-	this.title = title;
-	this.author = author;
-};
+class Book {
+	constructor(title, author) {
+		this.title = title;
+		this.author = author;
+	}
 
-Book.prototype.buy = function () {
-	console.log('Buy');
-};
+	buy() {
+		console.log('buy');
+	}
+}
 
-const AudioBook = function (title, author, lenMin) {
-	Book.call(this, title, author);
-	this.lenMin = lenMin;
-};
+class AudioBook extends Book {
+	constructor(title, author, lenMin) {
+		super(title, author);
+		this.lenMin = lenMin;
+	}
 
-AudioBook.prototype = Object.create(Book.prototype);
+	log() {
+		console.log(`${this.title} - ${this.lenMin}`)
+	}
+}
 
-AudioBook.prototype.log = function () {
-	console.log(`${this.title} - ${this.lenMin}`);
-};
-
-const book = new AudioBook('Lord of The Rings', 'Tolkien', 20 * 60);
-book.log();
+const book = new AudioBook('Lord of The Rings', 'Tolkien', 60 * 20);
 book.buy();
+book.log();
