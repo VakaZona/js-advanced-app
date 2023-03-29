c8e8406528197facc7c37fd2f131f754df44a7fc
Valery Zonov <valery.zonov@sibers.com>
2023-03-29 15:17:32 +0700

    promise

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
 'use strict';
 
-function req(id) {
-	const request = new XMLHttpRequest();
-	request.open('GET', 'https://dummyjson.com/products/' + id);
-	request.send();
-
-	request.addEventListener('load', function () {
-		const data = JSON.parse(this.responseText);
-		console.log(data);
-	});
-}
-req(1);
-req(2);
-req(3);
-
-
-console.log('end');
+const res = fetch('https://dummyjson.com/products/' + 1);
+console.log(res);
 
