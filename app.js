f47d4da47a2a2014bb551f38b09bc9f19ccf01e3
Valery Zonov <valery.zonov@sibers.com>
2023-03-29 15:25:55 +0700

    promises 2

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 'use strict';
 
-const res = fetch('https://dummyjson.com/products/' + 1);
-console.log(res);
+fetch('https://dummyjson.com/products/' + 1).then((response) => {
+	console.log(response);
+	return response.json();	
+}).then((data) => {
+	console.log(data);
+});
 
+//easy
+fetch('https://dummyjson.com/products/' + 1)
+	.then((response) => response.json())
+	.then((data) => 
+	console.log(data));
