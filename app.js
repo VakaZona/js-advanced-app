063e899382e797c4aafa74c57f672e9c0a6d1fbb
Valery Zonov <valery.zonov@sibers.com>
2023-03-29 16:22:53 +0700

    chain promise

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 'use strict';
 
-fetch('https://dummyjson.com/products/' + 1).then((response) => {
-	console.log(response);
-	return response.json();	
-}).then((data) => {
-	console.log(data);
-});
-
-//easy
-fetch('https://dummyjson.com/products/' + 1)
+fetch('https://dummyjson.com/products')
 	.then((response) => response.json())
-	.then((data) => 
-	console.log(data));
+	.then(({ products }) => {
+		console.log(products);
+		return fetch('https://dummyjson.com/products/' + products[0].id);
+	})
+	.then(response => response.json())
+	.then(data => {
+		console.log(data);
+	})
