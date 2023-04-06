3724004410ba07a42f706adde08d286acaa1ed49
Valery Zonov <valery.zonov@sibers.com>
2023-04-06 13:41:13 +0700

    combinations promise

--- app.js
+++ app.js
@@ -10,14 +10,29 @@ async function getProduct(id) {
 	return response.json();
 }
 
+async function getProductError(id) {
+	const response = await fetch('https://dummyjsons.com/products/' + id);
+	return response.json();
+}
+
 async function main() {
-	const { products } = await getAllProducts();
-	const res = await Promise.all(products.map(product => getProduct(product.id)));
-	console.log(res);
-	// for (const product of products) {
-	// 	const res = await getProduct(product.id);
-	// 	console.log(res);
-	// }
+	// const res1 = await Promise.all([
+	// 	getProduct(1),
+	// 	getProduct(2)
+	// ]);
+	// console.log(res1);
+
+	// const res2 = await Promise.allSettled([
+	// 	getProduct(1),
+	// 	getProductError(2)
+	// ]);
+	// console.log(res2);
+
+	const res3 = await Promise.race([
+		getProduct(1),
+		getProduct(2),
+	]);
+	console.log(res3);
 }
 
 main();
