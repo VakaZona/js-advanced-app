eea043b436d4cda716620ef54b76015614d60b09
Valery Zonov <valery.zonov@sibers.com>
2023-04-06 14:51:44 +0700

    scrollTo

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 'use strict';
 
-console.log(document.head);
+function generate(event) {
+	console.log(event.target.getBoundingClientRect())
 
+	console.log(window.pageXOffset);
+	console.log(window.pageYOffset);
+	console.log(document.documentElement.clientWidth);
+	console.log(document.documentElement.clientHeight);
 
-const el = document.querySelector('.wrapper');
-const el2 = document.querySelectorAll('meta');
-console.log(el);
-console.log(el2);
-const el3 = document.getElementsByClassName('wrapper');
-console.log(el3);
+	const el = document.querySelector('.down');
+	const rect = el.getBoundingClientRect();
 
-const button = document.createElement('button');
-button.innerHTML = 'Test';
+	window.scrollTo({
+		left: window.pageXOffset + rect.left,
+		top: window.pageYOffset + rect.top,
+		behavior: 'smooth'
 
-const button2 = document.createElement('button');
-button.innerHTML = 'Test2'
-
-el.append(button)
-el.prepend(button2)
-
-function generate() {
-	el.remove();
+	})
 }
