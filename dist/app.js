2813bfe57db579d9f15951033b779662f29b848f
Valery Zonov <valery.zonov@sibers.com>
2023-04-10 16:22:15 +0700

    search book

--- dist/app.js
+++ dist/app.js
@@ -1062,6 +1062,12 @@
 			this.state = state;
 		}
 
+		search() {
+			const value = this.el.querySelector('input').value;
+			this.state.searchQuery = value;
+
+		}
+
 		render() {
 			this.el.classList.add('search');
 			this.el.innerHTML = `
@@ -1076,6 +1082,12 @@
 			</div>
 			<button aria-label="Search"><img src="/static/search-white.svg" alt="search icon" /></button>
 		`;
+			this.el.querySelector('button').addEventListener('click', this.search.bind(this));
+			this.el.querySelector('input').addEventListener('keydown', (event) => {
+				if (event.code === 'Enter') {
+					this.search(); 
+				}
+			});
 			return this.el;
 		}
 	}
@@ -1092,7 +1104,22 @@
 			super();
 			this.appState = appState;
 			this.appState = onChange(this.appState, this.appStateHook.bind(this));
+			this.state = onChange(this.state, this.stateHook.bind(this));
 			this.setTitle('Search Book');
+		}
+
+		async stateHook(path) {
+			if (path === 'searchQuery') {
+				this.state.loading = true;
+				const data = await this.loadList(this.state.searchQuery, this.state.offset);
+				this.state.loading = false;
+				this.state.list = data;
+			}
+		}
+
+		async loadList(q, offset) {
+			const res = await fetch(`https://openlibrary.org/search.json?q=${q}&offset=${offset}`);
+			return res.json();
 		}
 
 		appStateHook(path) {
